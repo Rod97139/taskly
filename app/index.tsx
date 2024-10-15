2aef2a34cb55f31ad45c2ce456b18bcadb78a48a
Rod97139 <fayaflame@gmail.com>
2024-10-15 18:45:21 +0200

    add haptic feedback

--- app/index.tsx
+++ app/index.tsx
@@ -10,6 +10,7 @@ import { ShoppingListItem } from "../components/ShoppingListItem";
 import { theme } from "../theme";
 import { useEffect, useState } from "react";
 import { getFromStorage, saveToStorage } from "../utils/storage";
+import * as Haptics from "expo-haptics";
 
 const storageKey = "shopping-list";
 
@@ -20,30 +21,30 @@ type ShoppingListItemType = {
   lastUpdatedTimestamp: number;
 };
 
-const initialList: ShoppingListItemType[] = [
-  // { id: "1", name: "Coffee", lastUpdatedTimestamp: Date.now() },
-  // { id: "2", name: "Tea", lastUpdatedTimestamp: Date.now() },
-  // { id: "3", name: "Sugar", lastUpdatedTimestamp: Date.now() },
-  // { id: "4", name: "Milk", lastUpdatedTimestamp: Date.now() },
-  // { id: "5", name: "Bread", lastUpdatedTimestamp: Date.now() },
-  // { id: "6", name: "Butter", lastUpdatedTimestamp: Date.now() },
-  // { id: "7", name: "Eggs", lastUpdatedTimestamp: Date.now() },
-  // { id: "8", name: "Cheese", lastUpdatedTimestamp: Date.now() },
-  // { id: "9", name: "Yogurt", lastUpdatedTimestamp: Date.now() },
-  // { id: "10", name: "Juice", lastUpdatedTimestamp: Date.now() },
-  // { id: "11", name: "Fruit", lastUpdatedTimestamp: Date.now() },
-  // { id: "12", name: "Vegetables", lastUpdatedTimestamp: Date.now() },
-  // { id: "13", name: "Meat", lastUpdatedTimestamp: Date.now() },
-  // { id: "14", name: "Fish", lastUpdatedTimestamp: Date.now() },
-  // { id: "15", name: "Pasta", lastUpdatedTimestamp: Date.now() },
-  // { id: "16", name: "Rice", lastUpdatedTimestamp: Date.now() },
-  // { id: "17", name: "Beans", lastUpdatedTimestamp: Date.now() },
-];
+// const initialList: ShoppingListItemType[] = [
+//   { id: "1", name: "Coffee", lastUpdatedTimestamp: Date.now() },
+//   { id: "2", name: "Tea", lastUpdatedTimestamp: Date.now() },
+//   { id: "3", name: "Sugar", lastUpdatedTimestamp: Date.now() },
+//   { id: "4", name: "Milk", lastUpdatedTimestamp: Date.now() },
+//   { id: "5", name: "Bread", lastUpdatedTimestamp: Date.now() },
+//   { id: "6", name: "Butter", lastUpdatedTimestamp: Date.now() },
+//   { id: "7", name: "Eggs", lastUpdatedTimestamp: Date.now() },
+//   { id: "8", name: "Cheese", lastUpdatedTimestamp: Date.now() },
+//   { id: "9", name: "Yogurt", lastUpdatedTimestamp: Date.now() },
+//   { id: "10", name: "Juice", lastUpdatedTimestamp: Date.now() },
+//   { id: "11", name: "Fruit", lastUpdatedTimestamp: Date.now() },
+//   { id: "12", name: "Vegetables", lastUpdatedTimestamp: Date.now() },
+//   { id: "13", name: "Meat", lastUpdatedTimestamp: Date.now() },
+//   { id: "14", name: "Fish", lastUpdatedTimestamp: Date.now() },
+//   { id: "15", name: "Pasta", lastUpdatedTimestamp: Date.now() },
+//   { id: "16", name: "Rice", lastUpdatedTimestamp: Date.now() },
+//   { id: "17", name: "Beans", lastUpdatedTimestamp: Date.now() },
+// ];
 
 export default function App() {
   const [value, setValue] = useState("");
   const [shoppingList, setShoppingList] =
-    useState<ShoppingListItemType[]>(initialList);
+    useState<ShoppingListItemType[]>([]);
 
   useEffect(() => {
     const fetchInitialList = async () => {
@@ -77,12 +78,18 @@ export default function App() {
     const newShoppingList = shoppingList.filter((item) => item.id !== id);
     saveToStorage(storageKey, newShoppingList);
     LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
     setShoppingList(newShoppingList);
   };
 
   const handleToggleComplete = (id: string) => {
     const newShoppingList = shoppingList.map((item) => {
       if (item.id === id) {
+        if (item.completedAtTimestamp) {
+          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+        } else {
+          Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+        }
         return {
           ...item,
           lastUpdatedTimestamp: Date.now(),

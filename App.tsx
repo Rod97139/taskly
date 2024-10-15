93b4ddec8bd8c2319cea3726ee9c8b25d3fde264
Rod97139 <fayaflame@gmail.com>
2024-10-15 05:04:57 +0200

    create ShoppingListComponent

--- App.tsx
+++ App.tsx
@@ -1,39 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity, Alert } from "react-native";
-import { theme } from "./theme";
+import { StyleSheet, View } from "react-native";
+import { ShoppingListItem } from "./components/ShoppingListItem";
 
 export default function App() {
-  const handleDelete = () => {
-    Alert.alert(
-      "Are you sure you want to delete this?",
-      "It will be gone for good",
-      [
-        {
-          text: "Yes",
-          onPress: () => console.log("Delete confirmed"),
-          style: "destructive",
-        },
-        {
-          text: "No",
-          onPress: () => console.log("Delete cancelled"),
-          style: "cancel",
-        },
-      ],
-    );
-  };
   return (
     <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <Text style={styles.itemText}>Coffee</Text>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={handleDelete}
-          activeOpacity={0.8}
-        >
-          <Text style={styles.buttonText}>Delete</Text>
-        </TouchableOpacity>
-      </View>
-      <StatusBar style="auto" />
+      <ShoppingListItem name="Coffee"/>
+      <ShoppingListItem name="Tea"/>
+      <ShoppingListItem name="Sugar"/>
     </View>
   );
 }
@@ -43,29 +16,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     justifyContent: "center",
-  },
-  itemContainer: {
-    borderBottomWidth: 1,
-    borderBottomColor: "#1a759f",
-    paddingHorizontal: 8,
-    paddingVertical: 16,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  itemText: {
-    fontSize: 18,
-    fontWeight: "200",
-  },
-  button: {
-    backgroundColor: theme.colorBlack,
-    padding: 8,
-    borderRadius: 6,
-  },
-  buttonText: {
-    color: theme.colorWhite,
-    fontWeight: "bold",
-    textTransform: "uppercase",
-    letterSpacing: 1,
   },
 });
